8dd1fd31bac4f18a981b5c29872dd7b4a742e062
osamawaseem692 <osamawaseem692@gmail.com>
2023-03-06 20:32:25 +0500

    process .env added

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
-  console.log(req.url)
+  console.log(req.url);
   res.status(200).json({
     message: "Running",
   });
 });
 
-app.listen();
+app.listen(port, () => {
+  console.log("Server listening on port "+ port);
+});
